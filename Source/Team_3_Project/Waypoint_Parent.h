016833dae997e8327aeeda64fa5451f552af623a
Lucidity0207 <joshuabowden123@gmail.com>
2022-03-29 18:19:52 +0100

    JB - CPP for below commit

---
+++ Source/Team_3_Project/Waypoint_Parent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "Waypoint_Parent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM_3_PROJECT_API AWaypoint_Parent : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+};
